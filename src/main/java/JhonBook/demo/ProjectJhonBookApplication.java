0e5ef1ee46b92ab382b05108f4f07d990956d8b8
Jhonatan Santos <jhonatansantos085@gmail.com>
2024-11-17 20:07:48 -0300

    commit inicial

---
+++ src/main/java/JhonBook/demo/ProjectJhonBookApplication.java
@@ -0,0 +1,13 @@
+package JhonBook.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJhonBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJhonBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JhonBook/demo/ProjectJhonBookApplicationTests.java
@@ -0,0 +1,13 @@
+package JhonBook.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJhonBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
